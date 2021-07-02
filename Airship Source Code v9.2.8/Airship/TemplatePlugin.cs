b854c33d4e0ba1a7d6038b212bf2a3bfdbb6c830
Bossgamerteam <78909975+Bossgamerteam@users.noreply.github.com>
2021-07-02 10:43:59 +0100

    Add files via upload

---
+++ Airship Source Code v9.2.8/Airship/TemplatePlugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using BepInEx.Logging;
+using Reactor;
+using Reactor.Patches;
+
+namespace Airship
+{
+    [BepInPlugin(Id)]
+    [BepInProcess("Among Us.exe")]
+    [BepInDependency(ReactorPlugin.Id)]
+    public class Airship : BasePlugin
+    {
+        public const string Id = "boss.gamer.yt";
+
+        public Harmony Harmony { get; } = new Harmony(Id);
+        public static ManualLogSource log;
+
+
+        public override void Load()
+        {
+            log = Log;
+            
+            log.LogMessage("Better Airship Mod loaded");
+
+            ReactorVersionShower.TextUpdated += (text) =>
+            {
+                int index = text.text.LastIndexOf('\n');
+                text.text = text.text.Insert(index == -1 ? text.text.Length - 1 : index, 
+                    "\n \n \n \n \n \n \nBetter Airship by Boss_GamerYT & DaNoob \nyoutube.com/BossGamerYTShorts \n<color=#2cdb12>Version: 9.2.8</color> <color=#2cdb12>Stable</color> \n<color=#2294E6FF>Thanks for installing BetterAirship!</color>");
+            };
+            
+            Harmony.PatchAll();
+        }
+    }
+}
